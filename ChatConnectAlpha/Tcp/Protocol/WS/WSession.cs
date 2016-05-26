81abce14cb66bb3dd4e8b9c3c778254cf6b1c1ee
jackyt1988t <jackyt1988t@outlook.com>
2016-05-26 18:48:43 +0300

    update

---
+++ ChatConnectAlpha/Tcp/Protocol/WS/WSession.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+
+namespace ChatConnect.Tcp.Protocol.WS
+{
+	class WSession
+	{
+		/// <summary>
+		/// Продолжительность текущей сессии
+		/// </summary>
+		public TimeSpan Time
+		{
+			get
+			{
+				return DateTime.Now - Start;
+			}
+		}
+		/// <summary>
+		/// Начало текущей сессии
+		/// </summary>
+		public DateTime Start
+		{
+			get;
+		}
+		/// <summary>
+		/// Адресс подключения
+		/// </summary>
+		public IPAddress Address
+		{
+			get;
+		}
+
+		public WSession(IPAddress address)
+		{
+			Start =
+				DateTime.Now;
+			Address = address;
+		}
+	}
+}
