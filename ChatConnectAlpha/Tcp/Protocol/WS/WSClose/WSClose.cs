ccf10acf9e4fc2b95599ab9e7846f4c37f247e6c
jackyt1988t <jackyt1988t@gmail.com>
2016-05-27 00:06:03 +0300

    Update WSClose.cs

--- ChatConnectAlpha/Tcp/Protocol/WS/WSClose/WSClose.cs
+++ ChatConnectAlpha/Tcp/Protocol/WS/WSClose/WSClose.cs
@@ -2,18 +2,18 @@
 {
 	enum WSClose
 	{
-		Normal			  =	1000,
-		GoingAway		  =	1001,
-		ProtocolError     =	1002,
+		Normal		  = 1000,
+		GoingAway	  = 1001,
+		ProtocolError     = 1002,
 		UnsupportedData   = 1003,
-		Reserved		  = 1004,
+		Reserved	  = 1004,
 		NoStatusRcvd	  = 1005,
-		Abnormal		  =	1006,
+		Abnormal	  = 1006,
 		InvalidFrame	  = 1007,
 		PolicyViolation	  = 1008,
-		BigMessage		  = 1009,
-		Mandatory		  = 1010,
-		ServerError		  = 1011,
+		BigMessage	  = 1009,
+		Mandatory	  = 1010,
+		ServerError	  = 1011,
 		TLSHandshake	  = 1012
 	}
 }
