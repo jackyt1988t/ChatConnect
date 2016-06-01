309ffa3ee118e25a4b328091a5a286bc3e9ea2e5
jackyt1988t <jackyt1988t@outlook.com>
2016-06-01 20:07:30 +0300

    edit debug

--- MyWebSocket/Tcp/Protocol/WS/WSDebug/WSDebug.cs
+++ MyWebSocket/Tcp/Protocol/WS/WSDebug/WSDebug.cs
@@ -18,12 +18,23 @@ namespace MyWebSocket.Tcp.Protocol.WS
 			{
 				debug.Append(frame.DataHead[i].ToString("X") + " ");
 			}
+			debug.Append("[");
+			for (int i = 0; i < frame.DataHead.Length; i++)
+			{
+				string bin = Convert.ToString(frame.DataHead[i], 2);
+				while (bin.Length < 8)
+				{
+					bin = "0" + bin;
+				}
+				debug.Append(bin + " ");
+			}
+			debug.Append("]");
 			debug.AppendLine();
 			debug.AppendLine("FIND: " + frame.BitFin.ToString("X"));
 			debug.AppendLine("RSV1: " + frame.BitRsv1.ToString("X"));
 			debug.AppendLine("RSV2: " + frame.BitRsv2.ToString("X"));
 			debug.AppendLine("RSV3: " + frame.BitRsv3.ToString("X"));
-				debug.Append("PCOD: " + frame.BitPcod.ToString("X"));
+			  debug.Append("PCOD: 0x" + frame.BitPcod.ToString("X"));
 			switch (frame.BitPcod)
 			{
 				case WSFrameN13.TEXT:
@@ -54,7 +65,8 @@ namespace MyWebSocket.Tcp.Protocol.WS
 				debug.AppendLine();
 			else
 				debug.AppendLine(" --> " + frame.MaskVal.ToString());
-				debug.Append("LENG: " + frame.BitLeng.ToString("X"));
+			  debug.Append("LENG: 0x" + frame.BitLeng.ToString("X") +
+							 "(" +  frame.BitLeng.ToString()  + ")");
 			if (frame.BitLeng < 126)
 				debug.AppendLine();
 			else
@@ -82,12 +94,23 @@ namespace MyWebSocket.Tcp.Protocol.WS
 			{
 				debug.Append(frame.DataHead[i].ToString("X") + " ");
 			}
+			debug.Append("[");
+			for (int i = 0; i < frame.DataHead.Length; i++)
+			{
+				string bin = Convert.ToString(frame.DataHead[i], 2);
+				while (bin.Length < 8)
+				{
+					bin = "0" + bin;
+				}
+				debug.Append(bin + " ");
+			}
+			debug.Append("]");
 			debug.AppendLine();
 			debug.AppendLine("More: " + frame.BitMore.ToString("X"));
 			debug.AppendLine("RSV1: " + frame.BitRsv1.ToString("X"));
 			debug.AppendLine("RSV2: " + frame.BitRsv2.ToString("X"));
 			debug.AppendLine("RSV3: " + frame.BitRsv3.ToString("X"));
-			debug.Append("PCOD: " + frame.BitPcod.ToString("X"));
+			  debug.Append("PCOD: 0x" + frame.BitPcod.ToString("X"));
 			switch (frame.BitPcod)
 			{
 				case WSFrameSample.TEXT:
@@ -113,7 +136,8 @@ namespace MyWebSocket.Tcp.Protocol.WS
 					break;
 
 			}
-				debug.Append("LENG: " + frame.BitLeng.ToString("X"));
+			   debug.Append("LENG: 0x" + frame.BitLeng.ToString("X") +
+							    "(" + frame.BitLeng.ToString() + ")");
 			if (frame.BitLeng < 126)
 				debug.AppendLine();
 			else
