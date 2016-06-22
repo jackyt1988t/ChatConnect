78a263d4d2946379fd6eafce50d3cce1baf5597f
Anton <jackyt1988t@gmail.com>
2016-06-22 11:10:54 +0300

    Update UnitTest.cs

--- MyWebSocket/Test/UnitTestMyStream/UnitTestMyStream/UnitTest.cs
+++ MyWebSocket/Test/UnitTestMyStream/UnitTestMyStream/UnitTest.cs
@@ -10,18 +10,135 @@ namespace UnitTestMyStream
 		[TestMethod]
 		public void TestWrite()
 		{
+			bool error = false;
 			byte[] buffer = new byte[1024];
 			MyStream stream = new MyStream(1024);
-
+			
 			stream.Write(buffer, 0, 1024);
+
 			stream.Read(buffer, 0, 512);
+
 			stream.Write(buffer, 0, 512);
+
 			stream.Read(buffer, 0, 512);
 			stream.Read(buffer, 0, 512);
+
 			stream.Write(buffer, 0, 1024);
 
 			Assert.AreEqual(stream.Clear, 0, "Clear");
 			Assert.AreEqual(stream.Length, 1024, "Length");
+			try
+			{
+				strem.Read(null, 0, 0);
+			}
+			catch (ArgumentNullException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Возможна передача null buffer");
+			}
+			else
+				error = false;
+			try
+			{
+				strem.Read(new byte[1], -1, 0);
+			}
+			catch (ArgumentOutOfRangeException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Возможна передача ОТР. Значения pos");
+			}
+			else
+				error = false;
+			try
+			{
+				strem.Read(new byte[1], 0, -1);
+			}
+			catch (ArgumentOutOfRangeException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Возможна передача ОТР. Значения len");
+			}
+			else
+				error = false;
+			try
+			{
+				strem.Read(new byte[1], 0, 22);
+			}
+			catch (ArgumentOutOfRangeException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Нельзя выходить за границы переданного массива");
+			}
+			else
+				error = false;
+			try
+			{
+				strem.Write(null, 0, 0);
+			}
+			catch (ArgumentNullException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Возможна передача null buffer");
+			}
+			else
+				error = false;
+			try
+			{
+				strem.Write(new byte[1], -1, 0);
+			}
+			catch (ArgumentOutOfRangeException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Возможна передача ОТР. Значения pos");
+			}
+			else
+				error = false;
+			try
+			{
+				strem.Write(new byte[1], 0, -1);
+			}
+			catch (ArgumentOutOfRangeException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Возможна передача ОТР. Значения len");
+			}
+			else
+				error = false;
+			try
+			{
+				strem.Write(new byte[1], 0, 22);
+			}
+			catch (ArgumentOutOfRangeException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Нельзя выходить за границы переданного массива");
+			}
+			else
+				error = false;
 		}
 		[TestMethod]
 		public void TestLength()
@@ -30,14 +147,33 @@ namespace UnitTestMyStream
 			MyStream stream = new MyStream(1024);
 
 			stream.SetLength(1024);
+
 			stream.Position = 512;
+
 			stream.SetLength(512);
+
 			stream.Position = 512;
 			stream.Position = 512;
+
 			stream.SetLength(1024);
 
 			Assert.AreEqual(stream.Clear, 0, "Clear");
 			Assert.AreEqual(stream.Length, 1024, "Length");
+
+			try
+			{
+				strem.Position = -1;
+			}
+			catch (ArgumentOutOfRangeException err)
+			{
+				error = true;
+			}
+			if (!error)
+			{
+				Assert.Fail("Возможна передача ОТР. Значения value");
+			}
+			else
+				error = false;
 		}
 		[TestMethod]
 		public void TestResize()
